ec014cdb53c2346f7d5c044ea0864cc190fdb3f5
Salting <hzshenpanfei@corp.netease.com>
2017-05-26 20:51:54 +0800

    update C# class test

---
+++ cs_dll/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_dll
+{
+    public class Dog
+    {
+        static public void Type()
+        {
+            Console.WriteLine("a Dog!");
+        }
+        public void Bark()
+        {
+            Console.WriteLine("bark!");
+        }
+        public void Bark(int times)
+        {
+            for (var i = 0; i < times; ++i)
+                Console.WriteLine("bark!");
+        }
+    }
+}
