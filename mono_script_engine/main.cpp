efa20c02c33670c3514ed4fef0867dca165a09cb
Salting <hzshenpanfei@corp.netease.com>
2017-05-26 20:50:37 +0800

    delete C# dll

--- mono_script_engine/main.cpp
+++ mono_script_engine/main.cpp
@@ -27,6 +27,20 @@ int main()
 	mono_runtime_invoke(method, NULL, NULL, NULL);
 	mono_runtime_invoke(method, NULL, NULL, NULL);
 	mono_runtime_invoke(method, NULL, NULL, NULL);
+
+
+	MonoClass* dogclass = mono_class_from_name(image, "cs_dll", "Dog");
+	MonoObject* dogA = mono_object_new(domain, dogclass);
+	//Call its default constructor
+	mono_runtime_object_init(dogA);
+	MonoMethodDesc* BarkMethodDesc = mono_method_desc_new("Dog:Bark(int)", false); ;
+	MonoMethod* bark_method = mono_method_desc_search_in_class(BarkMethodDesc, dogclass);
+	//Set the arguments for the method
+	void* args[1];
+	int barkTimes = 3;
+	args[0] = &barkTimes;
+	mono_runtime_invoke(bark_method, dogA, args, NULL);
+
 	mono_jit_cleanup(domain);
 	return 0;
 }
